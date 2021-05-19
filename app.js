bde38242a452f99626564a46f8ec092dd3c57b79
tbrittain <treybrittain@gmail.com>
2021-05-19 02:21:14 -0500

    Configured persistent postgresql db with OWL info

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+})
 
