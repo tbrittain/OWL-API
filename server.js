d6d404714232dd32dc704d8cdef3dc3a238c58b3
tbrittain <treybrittain@gmail.com>
2021-05-20 02:11:16 -0500

    Conceptualizing endpoints

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server listening at http://localhost:${port}`);
 })
 
