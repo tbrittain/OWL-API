0a40a609250db3db1d2117b5698bdb6f7cccb77a
tbrittain <treybrittain@gmail.com>
2021-06-02 17:12:16 -0500

    Progress on TDD for updater container

--- dbUpdate/src/updater.py
+++ dbUpdate/src/updater.py
@@ -1,6 +1,17 @@
 import sys
+import utils
+import pandas as pd
+import os
+
+
+# TODO: analyze the CSV files and insert relevant info into each table
+# general idea: use sys.argv[argument index]
+# TODO: figure out a way to determine new players for insertion into the players_teams table
 
 if __name__ == "__main__":
-  a = int(sys.argv[1])
-  b = int(sys.argv[2])
-  print(a + b)
+    # a = int(sys.argv[1])
+    # b = int(sys.argv[2])
+    # print(a + b)
+    os.chdir("..")
+    players = pd.read_csv(f'{os.getcwd()}\\players.csv')
+    print(list(players.columns))
