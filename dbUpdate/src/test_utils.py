5de9df15a1716a282a4fd910813847f8f33b2e2d
tbrittain <treybrittain@gmail.com>
2021-06-03 02:53:28 -0500

    Created a database interfacing class

--- dbUpdate/src/test_utils.py
+++ dbUpdate/src/test_utils.py
@@ -2,6 +2,7 @@ import os
 import psycopg2
 import pytest
 from dotenv import load_dotenv
+
 import utils
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,22 +15,28 @@ db_name = os.getenv("DB_NAME")
 @pytest.fixture(scope="module")
 def db_connection():
     """Connect to database"""
-
-    print("Setup phase")
-    print(db_user, db_pass, db_name)
     conn = psycopg2.connect(dbname=db_name,
                             user=db_user,
                             password=db_pass,
                             host="localhost",
                             port=5432)
-    cur = conn.cursor()
 
     # Yield database connection to tests
     yield conn
 
-    print("Teardown phase")
     conn.rollback()
     conn.close()
+
+
+@pytest.fixture(scope="module")
+def db_connection_class():
+    """Connect to database through Database Connection object"""
+    db = utils.DatabaseConnection()
+
+    yield db
+
+    db.rollback()
+    db.terminate()
 
 
 def test_database_connection(db_connection):
@@ -42,6 +49,11 @@ def test_database_connection(db_connection):
     # raw rows data: [(9567,)]
     assert rows[0][0] > 0, "Existing data present in database"
     cur.close()
+
+
+def test_database_connection_class(db_connection_class):
+    pass
+    # TODO
 
 
 if __name__ == "__main__":
--- dbUpdate/src/utils.py
+++ dbUpdate/src/utils.py
@@ -1,4 +1,35 @@
-# TODO: create database connection in this file
+import os
+import psycopg2
+from dotenv import load_dotenv
 
-def batch_insert(table, columns, values_list):
-    pass
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(f'{base_dir}\\dev.env')
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASS")
+db_name = os.getenv("DB_NAME")
+
+
+class DatabaseConnection:
+    def __init__(self):
+        self.conn = psycopg2.connect(dbname=db_name,
+                                     user=db_user,
+                                     password=db_pass,
+                                     host="localhost",
+                                     port=5432)
+
+    def terminate(self):
+        self.conn.close()
+
+    def commit(self):
+        self.conn.commit()
+
+    def rollback(self):
+        self.conn.rollback()
+
+    def select_query(self, query, table):
+        cur = self.conn.cursor()
+        # TODO
+
+    def batch_insert(self, table, column_names, row_list):
+        cur = self.conn.cursor()
+        # TODO
